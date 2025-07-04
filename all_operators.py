0784e488c8aa2abf0e1095e4ee267db75e0647dc
Dinesh Chinthakuntla <dineshchinthakuntla@gmail.com>
2025-07-04 14:19:15 +0530

    alloperators

---
+++ all_operators.py
@@ -0,0 +1,149 @@
+#operators
+#arithimetic operators
+
+#Addition
+a=10
+b=5
+c=a+b
+print("ADDITION OF a+b is : ",c)
+
+#subtraction
+d=10
+e=5
+f=d-e
+print("SUBTRACTION OF d-e is : ",f)
+
+#Multiplication
+i=10
+j=5
+k=i*j
+print("MULTIPLICATION OF i*j is : ",k)
+
+#Division
+l=10
+m=5
+n=l/m
+print("DIVISION OF l/m is : ",n)
+
+#floor Division
+
+l=10
+m=5
+n=l//m
+print("DIVISION OF l//m is : ",n)
+
+#modulo 
+i=10
+j=5
+k=i%j
+print("MULTIPLICATION OF i % j is : ",k)
+
+#exponentiation
+
+i=2
+j=3
+k=i**j  # 2^3   (Two to the power of three)
+print("MULTIPLICATION OF i**j is : ",k)
+
+# ASSIGNMENT OPERATORS
+
+#assignment
+A=10
+print("value of A is :",A)
+
+#  += Add and assign 
+A=10
+B=20
+A+=B
+print(A)
+
+# -= Subtract and assign
+A=10
+B=20
+A-= B
+print(A)
+
+
+# *= multiply and assign 
+A=10
+B=20
+A*=B
+print(A)
+
+
+# /= division and assign 
+A=10
+B=20
+A/=B
+print(A)
+
+
+# //= Floor division and assign 
+A=10
+B=20
+A//=B
+print(A)
+
+
+# %= Modulo and assign 
+A=10
+B=20
+A%=B
+print(A)
+
+
+# **= exponentitation and assign 
+A=10
+B=20
+A**=B
+print(A)
+
+#comparission operators 
+#equal to operator
+A=10
+B=20
+print(A==B)
+
+#not equal to operator 
+
+A=10
+B=20
+print(A!=B)
+
+#greator than operator 
+
+A=10
+B=20
+print(A>B)
+
+#less than operator 
+A=10
+B=20
+print(A<B)
+
+#logical operators
+#OR Gate 
+A=True
+B=False
+print(A or B)
+
+# and gate 
+A=True
+B=True
+print(A and B)
+
+#not gate 
+A= True 
+B=not A
+print(B)
+
+# Membership Operators 
+#in operator 
+A=['apple', 'banana','pineapple','mango']
+print('apple' in A)
+
+#not in operator 
+A=['apple', 'banana','pineapple','mango']
+print('Sapota' not  in A)   #result will be true because sapost is not in the tuple in the given variable A
+
+
---
+++ assignment_operators.py
@@ -0,0 +1,5 @@
+a=999
+print("A value is : ",a)
+a+=10
+print(" a+=10 :",a)
+
