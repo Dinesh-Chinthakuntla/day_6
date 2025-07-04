fedd99b9774b453d1058127e59148e85643d2789
Dinesh Chinthakuntla <dineshchinthakuntla@gmail.com>
2025-07-04 13:41:33 +0530

    operators

---
+++ opeartors_add.py
@@ -0,0 +1,8 @@
+a=2505
+b=354
+c=34.5
+d=8.877
+f=a+b+c+d
+print(type(a),type(b),type(c),type(d))
+print(f)
+print(type(f))
---
+++ operators_subtraction.py
@@ -0,0 +1,8 @@
+a=234
+b=9876
+c=862.393
+d=88.2222
+f=a-b-c-d
+print(type(a),type(b),type(c),type(d))
+print(f)
+print(type(f))
